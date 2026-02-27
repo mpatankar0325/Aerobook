cdd080703ae1b54a834d128ce4fe88a95fbf65a6
Mitulkumar Patankar <mpatankar03@outlook.com>
2026-02-27 09:44:40 -0500

    Initial Commit

---
+++ Aerobook/AerobookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AerobookApp.swift
+//  Aerobook
+//
+//  Created by Mitul on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct AerobookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AerobookTests/AerobookTests.swift
@@ -0,0 +1,17 @@
+//
+//  AerobookTests.swift
+//  AerobookTests
+//
+//  Created by Mitul on 2/27/26.
+//
+
+import Testing
+@testable import Aerobook
+
+struct AerobookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
